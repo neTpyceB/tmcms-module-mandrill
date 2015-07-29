d0a475e120864e04de67573e8f661e0d05ac3086
Vadims Petrusevs <pvman@inbox.ru>
2015-07-29 13:19:19 +0300

    Init

---
+++ src/TMCms/Modules/Mandrill/Internal.php
@@ -0,0 +1,11 @@
+<?php
+namespace neTpyceB\TMCms\Modules\Mandrill;
+
+class Mandrill_Internal {
+    public function __construct(Mandrill $master) {
+        $this->master = $master;
+    }
+
+}
+
+
